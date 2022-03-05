7c057f7a2bebffe3d65ba1d0a3c181ea7c5cc83f
Alison8547 <alisonailson100@hotmail.com>
2022-03-05 12:24:35 -0300

    Questão-02

--- src/teste/Questao2Test.java
+++ src/teste/Questao2Test.java
@@ -5,7 +5,7 @@ import desafios.Questao2;
 public class Questao2Test {
     public static void main(String[] args) {
         // Testes
-        int[] vetor = {1, 5, 4, 3, 2,};
+        int[] vetor = {1, 5, 3, 4, 2,};
         int[] vetor2 = {1, 6, 2, 3, 5};
         System.out.println(Questao2.quantidadePares(vetor,2));
         System.out.println(Questao2.quantidadePares(vetor2,4));
